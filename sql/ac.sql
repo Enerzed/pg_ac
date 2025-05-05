170bde1378d72ec807037e92d8326e7ad8578e9f
Enerzed <lasker7241@gmail.com>
2025-05-05 23:35:12 +0700

    added some tests

--- sql/ac.sql
+++ sql/ac.sql
@@ -1,3 +1,9 @@
 CREATE EXTENSION pg_ac;
-SELECT ac_search(ac_build(to_tsvector('english', 'likes')), 'liked');
+SELECT ac_search(ac_build(to_tsvector('english', 'He likes dogs and cats')), to_tsquery('like & cat'));
+SELECT ac_search(ac_build(to_tsvector('english', 'He likes dogs')), to_tsquery('like & cat'));
+SELECT ac_search(ac_build(to_tsvector('english', 'He likes dogs')), to_tsquery('like & (cat | dog)'));
+SELECT ac_search(ac_build(to_tsvector('english', 'He likes snakes')), to_tsquery('like & (cat | dog)'));
+SELECT ac_search(ac_build(to_tsvector('english', 'He likes snakes')), to_tsquery('like & !(cat | dog)'));
+SELECT ac_search(ac_build(to_tsvector('english', 'He likes cats')), to_tsquery('like & !(cat | dog)'));
+
 DROP EXTENSION pg_ac;
