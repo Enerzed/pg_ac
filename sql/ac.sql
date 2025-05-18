9f26052a1f1a5b7347aee84832af7f8734b6ae5b
Enerzed <lasker7241@gmail.com>
2025-05-18 21:36:21 +0700

    remake tests

--- sql/ac.sql
+++ sql/ac.sql
@@ -3,67 +3,30 @@
 
 CREATE EXTENSION pg_ac;
 
-SELECT ac_build(to_tsvector('english', 'He likes dogs and cats'));
-SELECT ac_search(1, to_tsquery('like & cat'));
-
-SELECT ac_search
-(
-    ac_build(to_tsvector('english', 'He likes dogs and cats')),
-    to_tsquery('like & cat')
-);
-
-SELECT ac_search
-(
-    ac_build(to_tsvector('english', 'He likes dogs')),
-    to_tsquery('like & cat')
-);
-
-SELECT ac_search
-(
-    ac_build(to_tsvector('english', 'He likes dogs')),
-    to_tsquery('like & (cat | dog)')
-);
-
-SELECT ac_search
-(
-    ac_build(to_tsvector('english', 'He likes snakes')),
-    to_tsquery('like & (cat | dog)')
-);
-
-SELECT ac_search
-(
-    ac_build(to_tsvector('english', 'He likes cats')),
-    to_tsquery('like & !(cat | dog)')
-);
-
-SELECT ac_search
-(
-    ac_build(to_tsvector('english', 'He likes cats')),
-    to_tsquery('like & !(cat | dog)')
-);
-
-SELECT ac_search
-(
-    ac_build(to_tsvector('english', 'He likes cats')),
-    to_tsquery('likely')
-);
-
-SELECT ac_rank_simple
-(
-    ac_build(to_tsvector('The quick brown fox jumps over the lazy dog')),
-    'jump dog fox'
-);
-
-SELECT ac_rank_simple
-(
-    ac_build(to_tsvector('The quick brown fox jumps over the lazy dog')),
-    'fox'
-);
-
-SELECT ac_rank_simple
-(
-    ac_build(to_tsvector('The quick brown fox jumps over the lazy dog')),
-    'pink horse'
-);
+SELECT ac_build(to_tsvector('english', 'Dogs and cats are the best pets'));
+SELECT ac_build(to_tsvector('english', 'Dogs are the best pets'));
+SELECT ac_build(to_tsvector('english', 'Cats are the best pets'));
+SELECT ac_build(to_tsvector('english', 'Snakes are the best animals'));
+SELECT ac_build(to_tsvector('english', 'Quick brown fox jumps over the lazy dog'));
+/*Test first automaton*/
+SELECT ac_search(1, to_tsquery('english', 'dog & cat'));                        -- Should be true
+SELECT ac_search(1, to_tsquery('english', '!dog & !cat'));                      -- Should be false
+SELECT ac_search(1, to_tsquery('english', 'dog | snake'));                      -- Should be true
+/*Test second automaton*/
+SELECT ac_search(2, to_tsquery('english', 'pet & (cat | dog)'));                -- Should be true
+SELECT ac_search(2, to_tsquery('english', 'pet & !(cat | dog)'));               -- Should be false
+SELECT ac_search(2, to_tsquery('english', 'likely'));                           -- Should be false
+/*Test third automaton*/
+SELECT ac_search(3, to_tsquery('english', 'pet & (dog | cat)'));                -- Should be true
+SELECT ac_search(3, to_tsquery('english', 'pet & !(dog | cat)'));               -- Should be false
+SELECT ac_search(3, to_tsquery('english', ''));                                 -- Should be false
+/*Test fourth automaton*/
+SELECT ac_search(4, to_tsquery('english', 'are'));                              -- Should be false
+SELECT ac_search(4, to_tsquery('english', 'pet & (dog | cat)'));                -- Should be false
+SELECT ac_search(4, to_tsquery('english', 'the'));                              -- Should be false
+/*Test fifth automaton for ranked search*/
+SELECT ac_rank_simple(5, 'jump dog fox');                                       -- Should be 0.5
+SELECT ac_rank_simple(5,'fox');                                                 -- Should be 0.1(6)
+SELECT ac_rank_simple(5,'pink horse');                                          -- Should be 0
 
 DROP EXTENSION pg_ac;
